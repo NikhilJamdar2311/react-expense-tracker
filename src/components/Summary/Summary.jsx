3fcc61d3e1340188d56c4f1c36b1a461474a48bd
NikhilJamdar2311 <nikhilsjamdar@gmail.com>
2026-01-11 21:42:49 +0530

    Add Balance, Header, and Summary components with corresponding styles

---
+++ src/components/Summary/Summary.jsx
@@ -0,0 +1,15 @@
+function Summary(){
+    return(
+        <section className="summary">
+            <div className="summary-box income">
+                <h4>Income</h4>
+                <p>₹0</p>
+            </div>
+            <div className="summary-box expense">
+                <h4>Expense</h4>
+                <p>₹0</p>
+            </div>
+        </section>
+    )
+}
+export default Summary;
